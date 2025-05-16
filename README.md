34638ed1b10df19d1d066413c3ef85fbf5a65030
Marty Heyn <heynmarty@gmai.com>
2025-05-16 07:00:40 -0400

    open graph

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # ye-or
 
-Kanye has said / tweeted a totally normal amount in the past few months. I compiled some of the one liner bangers from his beautiful dark twisted mind and compiled them to create a simlpe to game. Test your knowledge and see if you can guess if a quote is from him, or one of his personal hero's from the past.
+Kanye has said / tweeted a totally normal amount in the past few months. I compiled some of the one liner bangers from his beautiful dark twisted mind to create a simlpe to game. Test your knowledge and see if you can guess if a quote is from him, or one of his personal hero's from the past.
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,28 @@ import "./styles/globals.css";
 import "./styles/animations.css";
 
 export const metadata: Metadata = {
-  title: "Or Hitler",
+  title: "Kanye or Hitler",
   description: "Who said it, Kanye or Hitler",
+  openGraph: {
+    title: 'Kanye or Hitler',
+    description: 'Who said it, Kanye or Hitler',
+    url: 'https://kanyeorhitler.xyz',
+    siteName: 'kanyeOrHitler',
+    images: [
+      {
+        url: 'https://dbzvyi28hj36i.cloudfront.net/kanye.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://dbzvyi28hj36i.cloudfront.net/kanye.png',
+        width: 1800,
+        height: 1600,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default async function RootLayout({
